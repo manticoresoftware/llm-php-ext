ceea9ca1b4dc4f52fea1332bae5463407d860dcc
Don Hardman <don.hardman@muvon.io>
2026-01-23 16:26:55 +0700

    build(config.m4): enable static Rust extension linking via stub file

---
+++ php_llm.h
@@ -0,0 +1,17 @@
+/*
+   +----------------------------------------------------------------------+
+   | LLM PHP Extension                                                    |
+   +----------------------------------------------------------------------+
+   | Copyright (c) Manticore Team                                         |
+   +----------------------------------------------------------------------+
+ */
+
+#ifndef PHP_LLM_H
+#define PHP_LLM_H
+
+extern zend_module_entry llm_module_entry;
+#define phpext_llm_ptr &llm_module_entry
+
+#define PHP_LLM_VERSION "0.1.0"
+
+#endif /* PHP_LLM_H */
